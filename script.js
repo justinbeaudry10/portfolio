9ae32200d1aa5b41549a483887fca32ef115de02
justinbeaudry10 <jbeaudr5@uwo.ca>
2022-03-15 18:33:28 -0400

    Added smooth scrolling nav

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-$(document).ready(function () {
-  $("nav a").on("click", function (event) {
-    event.preventDefault();
-    $("nav").addClass("fixed");
-    id = $(this).attr("href");
-    scrollVertical = $(id).offset().top;
+$(function () {
+  // Smooth scrolling
+  $(".nav_links").on("click", (e) => {
+    e.preventDefault();
 
-    $("body, html").animate({ scrollTop: scrollVertical });
-  });
-
-  $(document).on("scroll", function () {
-    secondPage = $("nav li:nth-child(2) a").attr("href");
-
-    if ($("body").scrollTop() >= $("nav").height()) {
-      $("nav").addClass("fixed");
-    } else {
-      $("nav").removeClass("fixed");
+    if ($(e.target).hasClass("nav_link")) {
+      let id = $(e.target).attr("href");
+      $(`${id}`)[0].scrollIntoView({ behavior: "smooth" });
     }
   });
+
+  const stickyNav = function (entries) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) {
+      $(".nav-sticky").removeClass("hidden");
+    } else {
+      $(".nav-sticky").addClass("hidden");
+    }
+  };
+
+  const homeObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `0px`,
+  });
+
+  homeObserver.observe($("#home")[0]);
 });
