df52f7f6df699a2cc93d45d43c32ae76d28b3863
justinbeaudry10 <jbeaudr5@uwo.ca>
2022-03-16 01:06:20 -0400

    Fading menu added

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(function () {
       $(`${id}`)[0].scrollIntoView({ behavior: "smooth" });
     }
   });
-
+  ////////////////////////////////////////////////////////
   // Sticky nav bar
   const stickyNav = function (entries) {
     const [entry] = entries;
@@ -27,4 +27,24 @@ $(function () {
   });
 
   homeObserver.observe($("#home")[0]);
+
+  ///////////////////////////////////////////////////////
+  // Nav fade animation
+  // const handleHover = function (e) {
+  //   if ($(e.target).hasClass("nav_link")) {
+  //     const siblings = e.target.closest("nav").querySelectorAll(".nav_link");
+
+  //     siblings.forEach((el) => {
+  //       if (el !== e.target) el.style.opacity = this;
+  //     });
+  //     // $(e.target)
+  //     //   .closest(".nav_link")
+  //     //   .each(function () {
+  //     //     console.log(e.target, this);
+  //     //   });
+  //   }
+  // };
+
+  // $(".nav").on("mouseover", handleHover.bind(0.5));
+  // $(".nav").on("mouseout", handleHover.bind(1));
 });
